c307adee3e96b4d351ab9897fe1268b5ad8ec54e
Igor Felipe <igor_colgate@hotmail.com>
2016-05-11 21:59:06 -0300

    Update mainwindow.cpp

--- Sources/mainwindow.cpp
+++ Sources/mainwindow.cpp
@@ -6,64 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle(titulo + "Notepad--");
-    connect(ui->actionAbrir, SIGNAL(triggered()), this, SLOT(openFile()));
-    connect(ui->actionNovo, SIGNAL(triggered()), this, SLOT(novoArquivo()));
-    connect(ui->actionSalvar, SIGNAL(triggered()), this, SLOT(saveFile()));
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
-void MainWindow::novoArquivo()
-{
-    titulo = "Novo Documento de Texto.txt";
-    this->setWindowTitle(titulo + " - Notepad--");
-    ui->plainTextEdit->setPlainText("");
-}
-
-void MainWindow::saveFile()
-{
-    std::ofstream newFile("teste");
-    if (newFile.is_open())
-    {
-        texto = ui->plainTextEdit->toPlainText();
-        newFile << texto.toStdString();
-        newFile.close();
-    }
-}
-
-void MainWindow::openFile()
-{
-    QString filePath = QFileDialog::getOpenFileName(this, tr("Open File"),
-                                                     "",
-                                                     tr("All Files (*)"));
-    QFile file(filePath);
-    ui->plainTextEdit->clear();
-    file.open(QIODevice::ReadOnly);
-    
-    QTextStream fileStream(&file);
-    QString line;
-    while(!fileStream.atEnd())
-    {
-        line = fileStream.readLine();
-        ui->plainTextEdit->appendPlainText(line);
-    }
-    file.close();
-    //ui->plainTextEdit->setPlainText(fileName);
-}
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
     this->setWindowTitle(nomeArquivo + "Notepad--");
-    novoArquivo();
+    newFile();
 
     if (texto != textoEspelho)
     {
@@ -71,7 +15,7 @@ MainWindow::MainWindow(QWidget *parent) :
     }
 
     connect(ui->actionAbrir, SIGNAL(triggered()), this, SLOT(openFile()));
-    connect(ui->actionNovo, SIGNAL(triggered()), this, SLOT(novoArquivo()));
+    connect(ui->actionNovo, SIGNAL(triggered()), this, SLOT(newFile()));
     connect(ui->actionSalvar, SIGNAL(triggered()), this, SLOT(saveFile()));
     connect(ui->actionSalvar_Como, SIGNAL(triggered()), this, SLOT(saveAs()));
     connect(ui->actionSair, SIGNAL(triggered()), this, SLOT(sair()));
@@ -82,7 +26,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::novoArquivo()
+void MainWindow::newFile()
 {
     editedOrNo = "Novo Documento de Texto.txt*";
     this->setWindowTitle(editedOrNo + " - Notepad--");
